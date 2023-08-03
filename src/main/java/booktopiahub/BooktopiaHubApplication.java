1cfc2db5dae680f2f25b61f396f357fbe2894963
Taras Machyta <taras.machyta.sa@gmail.com>
2023-08-03 13:04:56 +0300

    first commit

---
+++ src/main/java/booktopiahub/BooktopiaHubApplication.java
@@ -0,0 +1,13 @@
+package booktopiahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktopiaHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooktopiaHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/booktopiahub/BooktopiaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package booktopiahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktopiaHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
