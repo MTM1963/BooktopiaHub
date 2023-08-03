4fee5fd8f0c79d994037fc8c9b5286c9eb8fac9d
Taras Machyta <taras.machyta.sa@gmail.com>
2023-08-03 14:19:59 +0300

    Updates

--- src/test/java/booktopiahub/BooktopiaHubApplicationTests.java
+++ src/test/java/booktopiahub/BooktopiaHubApplicationTests.java
@@ -3,7 +3,7 @@ package booktopiahub;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BooktopiaHubApplicationTests.class)
 class BooktopiaHubApplicationTests {
 
     @Test
